2e5d87d5bb431204816e7f8abdbfe6661067a787
tmaja2020 <74681776+tmaja2020@users.noreply.github.com>
2025-11-11 14:28:46 -0600

    lhci autorun

--- .github/workflows/lighthouserc.js
+++ .github/workflows/lighthouserc.js
@@ -1,10 +1,22 @@
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: './',
+      staticDistDir: '../../',
     },
     upload: {
+
       target: 'temporary-public-storage',
+
+  
     },
+    "assert": {
+      "preset": "lighthouse:no-pwa",
+      "assertions": {
+        'categories:performance': ['warn', {minScore: 0.8}],
+        'categories:accessibility': ['warn', {minScore: 0.8}],
+        'categories:seo': ['warn', {minScore: 0.8}],
+        'categories:best-practices': ['warn', {minScore: 0.8}]
+      }
+    }
   },
 };
